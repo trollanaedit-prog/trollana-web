43a5f30c22c6052e7c4cf171c14bc5906a810f3c
trollanaedit-prog <trollanaedit@gmail.com>
2026-05-15 10:55:29 +0200

    Acualizacion

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4014479365" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4138839264" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
